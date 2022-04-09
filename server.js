83d058fd28a93cf5ce7b0494366e71fcb4934861
realVinayak <61122018+realVinayak@users.noreply.github.com>
2022-04-09 15:22:50 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,24 +6,36 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 // Create a route that will handle Twilio webhook requests, sent as an
 // HTTP POST to /voice in our application
-app.post('/', (request, response) => {
-  // Use the Twilio Node.js SDK to build an XML response
-  
+app.post('/name', (request, response) => {
   const tml = new VoiceResponse();
-  tml.say({ voice: 'alice' }, 'hello world!');
-  
+  tml.say({ voice: 'alice' }, 'What is your name?');
   tml.record({
-    action: '/name_record',
+    action: '/age',
     finishOnKey: 1,
     timeout: 10,
+    recordingStatusCallback: '/name_record'
+  });
+  response.type('text/xml');
+  response.send(tml.toString());
+});
+app.post('/age', (request, response) => {
+  const tml = new VoiceResponse();
+  tml.say({ voice: 'alice' }, 'What is your age');
+  tml.record({
+    action: '/',
+    finishOnKey: 1,
+    timeout: 10,
+    recordingStatusCallback: '/age_record'
   });
   response.type('text/xml');
   response.send(tml.toString());
 });
 app.post('/name_record', (req, res)=>{
-  console.log(req.RecordingUrl);
+  console.log(req);
 })
-
+app.post('/age_record', (req, res)=>{
+  console.log(req);
+})
 // Create an HTTP server and listen for requests on port 3000
 app.listen(PORT, () => {
   console.log(
