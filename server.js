3ca6efae49b3baae6bd0fe3e1db53128688ce8e1
realVinayak <61122018+realVinayak@users.noreply.github.com>
2022-04-09 13:32:41 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const PORT = process.env.PORT || 5000;
 // HTTP POST to /voice in our application
 app.post('/', (request, response) => {
   // Use the Twilio Node.js SDK to build an XML response
-  const twiml = new VoiceResponse();
-  twiml.say({ voice: 'alice' }, 'hello world! My name is Vinayak Jha');
-
-  // Render the response as XML in reply to the webhook request
-  response.type('text/xml');
-  response.send(twiml.toString());
+  
+  const response = new VoiceResponse();
+  response.record({
+      timeout: 10,
+      transcribe: true
+  });
+  
+  console.log(response.toString());
 });
 
 // Create an HTTP server and listen for requests on port 3000
