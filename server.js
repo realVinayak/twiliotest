74870b3abc9e826d19172bed3d3efde2b4244e87
realVinayak <61122018+realVinayak@users.noreply.github.com>
2022-04-09 16:12:51 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,13 +4,12 @@ const VoiceResponse = require('twilio').twiml.VoiceResponse;
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-// Create a route that will handle Twilio webhook requests, sent as an
-// HTTP POST to /voice in our application
+
 app.post('/name', (request, response) => {
   const tml = new VoiceResponse();
-  tml.say({ voice: 'alice' }, 'What is your name?');
+  tml.say({ voice: 'alice' }, 'What is your name');
   tml.record({
-    action: '/hangup',
+    action: '/age',
     finishOnKey: 1,
     timeout: 10,
     recordingStatusCallback: '/name_record',
@@ -25,18 +24,58 @@ app.post('/hangup', (request, response) => {
   response.type('text/xml');
   response.send(tml.toString());
 });
-/**app.post('/age', (request, response) => {
+app.post('/age', (request, response) => {
   const tml = new VoiceResponse();
   tml.say({ voice: 'alice' }, 'What is your age');
   tml.record({
-    action: '/',
+    action: '/location',
     finishOnKey: 1,
     timeout: 10,
-    recordingStatusCallback: '/age_record'
+    recordingStatusCallback: '/age_record',
+    recordingStatusCallbackMethod: 'GET'
   });
   response.type('text/xml');
   response.send(tml.toString());
-});**/
+});
+app.post('/location', (request, response) => {
+  const tml = new VoiceResponse();
+  tml.say({ voice: 'alice' }, 'Where do you currently live');
+  tml.record({
+    action: '/location_movement',
+    finishOnKey: 1,
+    timeout: 10,
+    recordingStatusCallback: '/location_record',
+    recordingStatusCallbackMethod: 'GET'
+  });
+  response.type('text/xml');
+  response.send(tml.toString());
+});
+app.post('/location_movement', (request, response) => {
+  const tml = new VoiceResponse();
+  tml.say({ voice: 'alice' }, 'Where do you wanna work, and how far are you willing to relocate');
+  tml.record({
+    action: '/qualifications',
+    finishOnKey: 1,
+    timeout: 10,
+    recordingStatusCallback: '/lm_record',
+    recordingStatusCallbackMethod: 'GET'
+  });
+  response.type('text/xml');
+  response.send(tml.toString());
+});
+app.post('/qualifications', (request, response) => {
+  const tml = new VoiceResponse();
+  tml.say({ voice: 'alice' }, 'What sort of job are you looking for, and what is your experience level');
+  tml.record({
+    action: '/hangup',
+    finishOnKey: 1,
+    timeout: 0,
+    recordingStatusCallback: '/qual_record',
+    recordingStatusCallbackMethod: 'GET'
+  });
+  response.type('text/xml');
+  response.send(tml.toString());
+});
 app.post('/name_record', (req, res)=>{
   console.log("Got name record now");
   console.log(req.body.AccountSid);
@@ -44,7 +83,7 @@ app.post('/name_record', (req, res)=>{
 app.post('/age_record', (req, res)=>{
   console.log(req);
 })
-// Create an HTTP server and listen for requests on port 3000
+
 app.listen(PORT, () => {
   console.log(
     'Now listening on port 3000. ' +
