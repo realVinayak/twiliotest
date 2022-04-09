b3dd49bdae5480ab04759e8050384fac394509bd
realVinayak <61122018+realVinayak@users.noreply.github.com>
2022-04-09 14:56:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const VoiceResponse = require('twilio').twiml.VoiceResponse;
+const 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 // Create a route that will handle Twilio webhook requests, sent as an
 // HTTP POST to /voice in our application
-app.post('/', (request, response) => {
+app.post('/voice', (request, response) => {
   // Use the Twilio Node.js SDK to build an XML response
   
-  const response_1 = new VoiceResponse();
-  response_1.record({
-      timeout: 10,
-      transcribe: true
+  const response1 = new VoiceResponse();
+  response1.record({
+    action: '/name_record',
+    finishOnKey: 1,
+    timeout: 10,
   });
   
-  console.log(response_1.toString());
+  console.log(response1.toString());
 });
+app.get('/name_record', (req, res)=>{
+  console.log(req);
+})
 
 // Create an HTTP server and listen for requests on port 3000
 app.listen(PORT, () => {
