2985bb4112b0a00f3510d59ed15e000afdd93e0f
realVinayak <realvinayak123@gmail.com>
2022-04-10 04:03:17 -0500

    mccd

--- server.js
+++ server.js
@@ -46,20 +46,6 @@ const linkSchema = new mongoose.Schema({
 })
 const links_ = mongoose.model('link_ae', linkSchema)
 const app = express();
-const app2 = express();
-
-
-var server = http.createServer(app2)
-server.listen(80)
-
-console.log("http server listening on %d", 80)
-console.log("websocket server created")
-
-wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)})
-
 const PORT = process.env.PORT || 5000;
 app.post('/join', (request, response) => {
   const tml = new VoiceResponse();
